83a8d08605a9a784c4be6cad52b0231621e1f7b5
seymourgabe <seymourgabe@gmail.com>
2019-08-16 12:54:09 -0400

    Added morph.hex to /bin/

--- src/herd.c
+++ src/herd.c
@@ -1,25 +1,26 @@
 #include <kilolib.h>
 
-uint32_t counter = 0;
+uint32_t counter = 0; // Global counter
+
 void setup()
 {
 }
 
 void loop()
 {
-    //set_motors(kilo_straight_left, kilo_straight_left);
-    counter++;
-    set_color(RGB(1, 0, 0));
-    set_motors(150,150);
-    delay(400);
-    if (counter % 15 == 0) {
-        set_motors(0, 0);
-        set_color(RGB(1, 1, 1));
-        delay(5000);
+    
+    ++counter;			// Increment
+    set_color(RGB(1, 0, 0));	// Turn red
+    set_motors(150,150);	// Found these motor values work well. Not too strong but enough to move them
+    delay(400);			// Just spin for some time
+    if (counter % 15 == 0) {	// Every 15 increments
+        set_motors(0, 0);	// Turn the motors off
+        set_color(RGB(1, 1, 1));// Turn the lights white
+        delay(5000);		// Pause for some time
     } else {
-        set_motors(kilo_straight_left, kilo_straight_left); 
+        set_motors(kilo_straight_left, kilo_straight_left); // Let them walk a little bit
     }
-    delay(400);
+    delay(400);			// Pause for more time
 }
 
 
